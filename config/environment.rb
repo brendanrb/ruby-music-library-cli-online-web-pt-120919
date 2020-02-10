522ee4f80ec4a864af72f85f8215364edbb16761
Brendan R Borgers <brendanborgers@gmail.com>
2020-02-10 16:01:30 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,15 @@ module Concerns
 end
 
 require_all 'lib'
+
+module Concerns::Findable
+
+  def find_by_name(name)
+    self.all.detect{|i| i.name == name}
+  end
+
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+end
+
--- spec/001_song_basics_spec.rb
+++ spec/001_song_basics_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'pry'
 
 describe "Song" do
   let(:song) { Song.new("In the Aeroplane Over the Sea") }
@@ -73,3 +74,336 @@ describe "Song" do
     end
   end
 end
+
+
+
+class Song
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+
+  @@all = [] #this is a collection of all of the songs
+
+  def initialize(name,artist=nil,genre=nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap do|song|
+      song.save
+    end
+  end
+
+  def artist=(artist)
+    if @artist == nil
+      @artist = artist
+    else
+      @artist = @artist
+    end
+    if self.artist != nil
+      @artist.add_song(self)
+    end
+    @artist
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre=(genre)
+    if @genre == nil
+      @genre = genre
+    else
+      @genre= @genre
+    end
+    if self.genre != nil
+      @genre.add_song(self)
+    end
+    @genre
+
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |song|
+        song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+  def self.create_from_filename(name)
+    @@all << self.new_from_filename(name)
+
+  end
+
+  def self.new_from_filename(name)
+    #parse the filename
+    song_name = name.split(" - ")[1]
+    artist_name = name.split(" - ")[0]
+    genre_name = name.split(" - ")[2].chomp(".mp3")
+    song = self.find_or_create_by_name(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = Genre.find_or_create_by_name(genre_name)
+    song
+  end
+
+
+end
+
+
+class Artist
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap do |artist|
+      artist.save
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
+  end
+
+  #artist has many genres through songs
+  def genres
+    genres = @songs.collect do |song|
+      song.genre
+    end
+    genres.uniq
+  end
+
+
+
+end
+
+class Genre
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap do |genre|
+      genre.save
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+
+  def add_song(song)
+    song.genre = self unless song.genre == self
+    @songs << song unless @songs.include?(song)
+  end
+
+
+  def artists
+    artists = @songs.collect do |song|
+      song.artist
+    end
+    artists.uniq
+  end
+
+end
+
+
+class MusicImporter
+
+  def initialize(path)
+    @path = path
+  end
+
+  def path
+    @path
+  end
+
+  def files()
+    @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+  end
+
+  def import()
+    files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end
+
+end
+
+class MusicLibraryController
+
+  extend Concerns::Findable
+
+  def initialize(path = "./db/mp3s")
+    new_importer_object = MusicImporter.new(path)
+    new_importer_object.import
+  end
+
+  def call
+    input = ""
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip.downcase
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+
+    end
+  end
+
+   def list_songs
+     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+
+  def list_artists
+    songs_sorted_by_artist = Artist.all.sort_by do |artist|
+      artist.name
+    end
+    songs_sorted_by_artist.each.with_index(1) do |artist,index|
+      puts "#{index}. #{artist.name}"
+    end
+  end
+
+  def list_genres
+    songs_sorted_by_genre = Genre.all.sort_by do |genre|
+      genre.name
+    end
+    songs_sorted_by_genre.each.with_index(1) do |genre,index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
+
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    if artist = Artist.find_by_name(input)
+      songs_sorted_by_name = artist.songs.sort_by do |song|
+        song.name
+      end
+      songs_sorted_by_name.each.with_index(1) do |song,index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+    if genre = Genre.find_by_name(input) 
+      songs_sorted_by_name = genre.songs.sort_by do |song|
+        song.name
+      end
+      songs_sorted_by_name.each.with_index(1) do |song,index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = list_of_songs[input+2]
+      puts "Playing #{song.name} by #{song.artist.name}"
+    
+    end
+  end
+
+end
+
+
+
+
+
+
